8c591734d4be09e7d28497f99f05107c714c31bd
Piyushnmj <piyushnmj@yahoo.com>
2022-10-13 17:38:26 +0530

    Employee wages for 100 hours or 20 days

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -4,14 +4,16 @@
     public const int IS_PART_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 100;
     public const int NUM_OF_WORKING_DAYS = 20;
+    public const int MAX_HRS_IN_MONTH = 100;
     static void Main(string[] args)
     {
         int empHrs = 0;
-        int empWage = 0; 
-        int totalEmpWage = 0;
+        int totalEmpHrs = 0; 
+        int totalWorkingDays = 0;
 
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             switch (empCheck)
@@ -26,10 +28,10 @@
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            Console.WriteLine("Employee Wage: " + empWage);
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Day#:" + totalWorkingDays + "Employee Hrs: " + empHrs);
         }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         Console.WriteLine("Total Employee Wage: " + totalEmpWage);
     }
 }
