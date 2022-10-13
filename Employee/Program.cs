c01cf4b2720394de2ef53c1b2cd7de275fec8b30
Piyushnmj <piyushnmj@yahoo.com>
2022-10-13 16:45:15 +0530

    Wage of part time employee

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -2,18 +2,23 @@
 {
     static void Main(string[] args)
     {
-        //Constants
+        
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 100;
-        //Variables
+        
         int empHrs = 0;
         int empWage = 0;
         Random random = new Random();
-        //Computation
-        int empCheck = random.Next(0, 2);
+        
+        int empCheck = random.Next(0, 3);
         if (empCheck == IS_FULL_TIME)
         {
             empHrs = 8;
+        }
+        else if (empCheck == IS_PART_TIME)
+        {
+            empHrs = 4;
         }
         else
         {
