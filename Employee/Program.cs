66013e616cc1de67dd4cb9746e2b7b028cdfccf1
Piyushnmj <piyushnmj@yahoo.com>
2022-10-14 11:26:28 +0530

    UC9: Employee Total Wage by Company

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -1,41 +1,12 @@
 ﻿class Program
 {
-    public const int IS_PART_TIME = 1;
-    public const int IS_FULL_TIME = 2;
-    public const int EMP_RATE_PER_HOUR = 100;
-    public const int NUM_OF_WORKING_DAYS = 20;
-    public const int MAX_HRS_IN_MONTH = 100;
-
-    public static int computeEmpWage()
+   static void Main(string[] args)
     {
-        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-        {
-            totalWorkingDays++;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs += empHrs;
-            Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs: " + empHrs);
-        }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Total Emp Wage: " + totalEmpWage);
-        return totalEmpWage;
-    }
-    static void Main(string[] args)
-    {
-        computeEmpWage();
+        EmpWageBuilderObject Microsoft = new EmpWageBuilderObject("Microsoft", 100, 20, 100);
+        EmpWageBuilderObject Google = new EmpWageBuilderObject("Google", 200, 30, 150);
+        Microsoft.computeEmpWage();
+        Console.WriteLine(Microsoft.toString());
+        Google.computeEmpWage();
+        Console.WriteLine(Google.toString());
     }
 }
