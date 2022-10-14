832b7b6d326896ca1e1909351a65f0c686d98406
Piyushnmj <piyushnmj@yahoo.com>
2022-10-14 09:04:16 +0530

    refactor

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -1,15 +1,14 @@
 ﻿class Program
 {
-    public const int IS_FULL_TIME = 1;
-    public const int IS_PART_TIME = 2;
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 100;
     public const int NUM_OF_WORKING_DAYS = 20;
     public const int MAX_HRS_IN_MONTH = 100;
-    static void Main(string[] args)
+
+    public static int computeEmpWage()
     {
-        int empHrs = 0;
-        int totalEmpHrs = 0; 
-        int totalWorkingDays = 0;
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
@@ -18,20 +17,25 @@
             int empCheck = random.Next(0, 3);
             switch (empCheck)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
                 case IS_PART_TIME:
                     empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
                 default:
                     empHrs = 0;
                     break;
             }
             totalEmpHrs += empHrs;
-            Console.WriteLine("Day#:" + totalWorkingDays + "Employee Hrs: " + empHrs);
+            Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs: " + empHrs);
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Total Employee Wage: " + totalEmpWage);
+        Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+        return totalEmpWage;
+    }
+    static void Main(string[] args)
+    {
+        computeEmpWage();
     }
 }
